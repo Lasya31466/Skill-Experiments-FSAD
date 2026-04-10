0e9bc28ae67ba457a31aa3a4dd64cb441b79045b
Lasya31466 <2400031466@kluniversity.in>
2026-04-10 20:22:13 +0530

    commit

--- skill-12/src/main/java/com/klu/skill_12/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klu.skill_12.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.skill_12.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
